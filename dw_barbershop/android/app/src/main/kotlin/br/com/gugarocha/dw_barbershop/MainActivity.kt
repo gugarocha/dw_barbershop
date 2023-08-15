bdd89712110fdc0381ad60884b85d44d7b5811ea
Gustavo <guga.rocha.silveira@hotmail.com>
2023-08-14 22:55:57 -0300

    init project

---
+++ dw_barbershop/android/app/src/main/kotlin/br/com/gugarocha/dw_barbershop/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gugarocha.dw_barbershop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dw_barbershop/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'src/barbershop_app.dart';
+
+void main() {
+  runApp(const BarbershopApp());
+}
+
